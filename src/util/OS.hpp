96bf8a9cfb922975fb67a797ee8017c6f4ad2d45
Eddie Davis <eddiedavis@boisestate.edu>
2019-06-14 09:57:16 -0600

    Added OS utility class.

---
+++ src/util/OS.hpp
@@ -0,0 +1,46 @@
+#ifndef _OS_HPP_
+#define _OS_HPP_
+
+#include <cstdio>
+#include <cstdlib>
+#include <unistd.h>
+#include <string>
+using std::string;
+
+namespace util {
+class OS {
+public:
+string OS::run(const string& cmd) {
+	char buffer[BUFFSIZE];
+	string result = "";
+
+	FILE* pipe = popen(cmd.c_str(), "r");
+	if (pipe) {
+		while (!feof(pipe)) {
+			if (fgets(buffer, BUFFSIZE, pipe) != NULL) {
+				result += string(buffer);
+            }
+		}
+		pclose(pipe);
+	}
+
+	return result;
+}
+
+int OS::ncpus() {
+    int ncpus = 1;
+
+#ifdef _SC_NPROCESSORS_ONLN
+    ncpus = sysconf(_SC_NPROCESSORS_ONLN);
+#endif
+
+    return ncpus;
+}
+
+private:
+    const int BUFFSIZE 1024;
+};
+}
+
+#endif
+
