e57de003ba1f7c1eedd79d6482b5ccad072b4bf5
Eddie Davis <eddiedavis@boisestate.edu>
2019-09-12 10:27:13 -0600

    Updated TACO test.

--- test/TensAlgCompTest.cpp
+++ test/TensAlgCompTest.cpp
@@ -7,10 +7,6 @@ using namespace testing;
 #include <taco.h>
 using namespace taco;
 
-#include <util/MatrixIO.hpp>
-using util::MatlabIO;
-using util::TensorIO;
-
 #include "BenchmarkTest.hpp"
 
 namespace test {
@@ -37,45 +33,46 @@ namespace test {
             // Load a sparse matrix from file (stored in the Matrix Market format) and
             // store it as a compressed sparse row matrix. Matrices correspond to order-2
             // tensors in taco.
-            Tensor<double> A = read(filename, csr);
+            //Tensor<double> A = read(filename, csr);
+            Tensor<double> A({4, 4}, csr);
 
             Tensor<double> B({A.getDimension(0), A.getDimension(1)}, csr);
-            for (int i = 0; i < B.getDimension(0); ++i) {
-                for (int j = 0; j < B.getDimension(1); ++j) {
-                    B.insert({i,j}, 1.0);
-                }
-            }
-            B.pack();
+            InitMatrix(B, 1.0);
 
+            Tensor<double> C({A.getDimension(0), A.getDimension(1)}, csr);
+            InitMatrix(C);
 
+            IndexVar i, j; //, k, l;
+            // Hadamard product:
+            C(i,j) = A(i,j) * B(i,j);
+
+            C.compile();
+
+            // We can now call the functions taco generated to assemble the indices of the
+            // output matrix and then actually compute the MTTKRP.
+            C.assemble();
+            C.compute();
+
+            // Write the output of the computation to file (stored in the FROSTT format).
+            write("C.tns", C);
         }
 
-        void Init()
+        void InitMatrix(Tensor<double>& mtx, double val = 0.0) {
+            for (int i = 0; i < mtx.getDimension(0); ++i) {
+                for (int j = 0; j < mtx.getDimension(1); ++j) {
+                    mtx.insert({i,j}, 1.0);
+                }
+            }
+            //mtx.pack();
+        }
 
         virtual void TearDown() {
         }
 
-
-//        double* _opts;
-//        splatt_idx_t _dim;
-//        splatt_csf* _tns;
-//        splatt_kruskal _fac;
     };
 
-    TEST_F(TensAlgCompTest, SplattCPD) {
-        SetUp("../../matrices/impact.mtx");
-        /* do the factorization! */
-//        int ret = splatt_cpd_als(_tns, _rank, _opts, &_fac);
-//
-//        /* do some processing */
-//        for(splatt_idx_t m = 0; m < _dim; ++m) {
-//            /* access factored.lambda and factored.factors[m] */
-//        }
-    }
+    TEST_F(TensAlgCompTest, Convert) {
+        SetUp("./data/matrix/impact.mtx");
 
-    TEST_F(TensAlgCompTest, CPD_ALS) {
-        SetUp("./data/tensor/matmul_5-5-5.tns", 10);
-
-        int stop = 1;
     }
 }
